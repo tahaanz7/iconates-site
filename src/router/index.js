20a1589de323f2b2c73db9709b217f7a9bd7054e
Taha Anz <tahaanz70@gmail.com>
2025-10-09 17:30:13 +0100

    add router, license page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/HomeView.vue'
+import IconsView from '../views/IconsView.vue'
+import DocsView from '../views/DocsView.vue'
+import AboutView from '../views/AboutView.vue'
+import ContributeView from '../views/ContributeView.vue'
+import LicenseView from '../views/LicenseView.vue'
 
 const routes = [
- 
+  { path: '/', component: HomeView },
+  { path: '/icons', component: IconsView },
+  { path: '/docs', component: DocsView },
+  { path: '/about', component: AboutView },
+  { path: '/contribute', component: ContributeView },
+  { path: '/license', component: LicenseView }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 })
 
 export default router

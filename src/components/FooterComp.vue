74e65bb5409e5f88273a4e51cb3e5fa3d8b18ec6
Taha Anz <tahaanz70@gmail.com>
2025-10-09 17:40:34 +0100

    footer + nabar

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="text-center py-6 text-sm text-gray-500 border-t">
+      MIT License • Made with ❤️ by Iconates
+    </footer>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="w-full py-4 px-4 mt-2">
+    <div class="max-w-6xl mx-auto flex justify-between items-center  border-b border-gray-200 pb-4">
+      <RouterLink to="/"><h1 class="text-2xl font-bold">Iconates</h1></RouterLink>
+      <ul class="flex gap-6 text-gray-600">
+        <li><RouterLink to="/icons">Icons</RouterLink></li>
+        <li><RouterLink to="/docs">Docs</RouterLink></li>
+        <li><RouterLink to="/contribute">Contribute</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+        <li><RouterLink to="/license">License</RouterLink></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>

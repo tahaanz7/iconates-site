834dffe6d2e1153a7681772945fbff9595720031
Taha Anz <tahaanz70@gmail.com>
2025-10-09 17:35:24 +0100

    fixing home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,37 @@
+<script setup lang="ts">
+import HeroSection from "../components/HeroSection.vue";
+import IconGrid from "../components/IconGrid.vue";
+</script>
+
 <template>
-  <div class="p-8 text-center">
-    <h1 class="text-3xl font-bold mb-2">Page Name</h1>
-    <p>Coming soon...</p>
+  <div class="min-h-screen max-w-6xl mx-auto mb-20">
+    <HeroSection />
+    <section>
+      <IconGrid />
+    </section>
+
+    
+
+    <!-- meet the creator -->
+   <section class="flex flex-col items-center justify-center text-center py-16 bg-gray-50 rounded-lg mt-20 w-max mx-auto px-40">
+   
+    <img
+      src="../../public/images/taha-aanizile.jpg"
+      alt="Taha Aanizile"
+      class="w-32 h-32 rounded-full object-cover mb-4 shadow-md"
+    />
+
+   
+    <h2 class="text-2xl font-bold mb-1">Taha Aanizile</h2>
+    <p class="text-gray-500 mb-6">Creator & Maintainer of Iconates <br> Software Engineer / Designer</p>
+
+   
+    <div class="flex gap-6">
+      <div>
+        linkedin
+      </div>
+      <div>Github</div>
+    </div>
+  </section>
   </div>
 </template>
